c5bf20c759e15c0602d0a59ed9a71f601af137f0
shaddock <m15926539316@163.com>
2022-03-17 15:12:42 +0800

    hot-fix commit

--- src/main/java/edu/study/book/chapter6/lambda/CommandTest.java
+++ src/main/java/edu/study/book/chapter6/lambda/CommandTest.java
@@ -3,6 +3,7 @@ package edu.study.book.chapter6.lambda;
 public class CommandTest {
     public static void main(String[] args) {
         System.out.println("第二次提交");
+        System.out.println("hot-fix");
         process(new int[]{1, 2, 3}, (int element)->{
             System.out.println(element);
         });
